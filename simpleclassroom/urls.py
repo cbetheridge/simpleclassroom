7df5f871d6423c02972384cf4df8e7c4aa7ddbde
Bryce Etheridge <cbetheridge@gmail.com>
2016-04-14 20:27:55 -0500

    Refactoring away from django's app structure

--- simpleclassroom/urls.py
+++ simpleclassroom/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from classroom import views
+from views import views
 
 urlpatterns = [
   url(r'^$', views.display_index, name='index'),
